df2aa859e40b414a8f72ced396e550f99dc6ede4
Laura <lau@gmail.com>
2018-02-28 14:07:00 +0100

    [FEAT]trabajando en la comunicacion entre componentes

--- src/pages/welcome/getStarted/getStarted.ts
+++ src/pages/welcome/getStarted/getStarted.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController} from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { Welcome } from '../welcome';
 
 
 @Component({
   templateUrl: 'getStarted.html'
-  
+
 })
 export class GetStarted {
-  
-  constructor(public navCtrl: NavController) {
-    
-  }
-  goTo() {
-    this.navCtrl.push(Welcome);
-    
-  }
-}
 
+  user: { 'name': string; 'password': string; };
+  //@Output() sign = new EventEmitter();
+  // sign;
+  constructor(public navCtrl: NavController) {
+    this.user =
+      {
+        'name': '',
+        'password': ''
+      };
+    //this.sign = false;
+  }
+  signUp(user) {
+    //this.sign = true;
+    this.navCtrl.push(Welcome, { user: this.user });
+  }
+  cancel() {
+    this.navCtrl.push(Welcome);
+  }
+
+}
--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,19 +1,26 @@
 
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { GetStarted } from './getStarted/getStarted';
+//import { ProfileTab } from './tabs/tab-profile';
+//import { ControlTab } from './tabs/tab-control';
 
 @Component({
-    selector: 'app.scss', // estilo de home.scss
-    templateUrl: 'welcome.html'
-  })
-  export class Welcome {
-      constructor(public navCtrl: NavController) {
-    
-      }
-
-      openGetStartedPage() {
-        this.navCtrl.push(GetStarted);
-        
-      }
+  selector: 'app.scss', // estilo de home.scss
+  templateUrl: 'welcome.html'
+})
+export class Welcome {
+  user;
+  constructor(public navCtrl: NavController, public params: NavParams) {
+    this.user = this.params.get('user');
+    console.log(this.user);
   }
+
+  openGetStartedPage() {
+    this.navCtrl.push(GetStarted);
+
+  }
+  //tab2Root = ProfileTab;
+  //tab3Root = ControlTab;
+
+}
