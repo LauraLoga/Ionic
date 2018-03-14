bce9447db322298bc924d72ff24e5cf781c6599d
Laura <lau@gmail.com>
2018-03-14 12:18:19 +0100

    Ionic view

--- src/pages/welcome/tabs/tab-profile.ts
+++ src/pages/welcome/tabs/tab-profile.ts
@@ -8,27 +8,30 @@ import { NavParams } from 'ionic-angular';
   templateUrl: 'tab-profile.html' })
 export class ProfileTab implements OnInit {
   devices: Array<{ title: string; description: string }>;
-  col;
   name;
   showInput = false;
+  title: any;
+  descr: any;
 
   constructor(public params: NavParams, private storage: Storage) {
-    this.devices = [
-      { title: 'Greenhouse', description: 'The main objective is to visualize in our phones a platform that monitoring and controlling' },
-      { title: 'AEMET', description: 'The weather' },
-      { title: 'Remote house', description: 'Robotic house' },
-      { title: 'Greenhouse 2', description: 'The main objective is to visualize in our phones a platform that monitoring and controlling' }
-    ];
-    this.col = this.devices.length;
+
   }
 
   ngOnInit(): void {
     this.name = this.storage.get('name').then((val) => {
       this.name = val;
     });
+    this.devices = [
+      { title: 'Greenhouse', description: 'The main objective is to visualize in our phones a platform that monitoring and controlling' },
+      { title: 'AEMET', description: 'The weather' },
+      { title: 'Remote house', description: 'Robotic house' },
+      { title: 'Greenhouse 2', description: 'The main objective is to visualize in our phones a platform that monitoring and controlling' }
+    ];
   }
   addDevice(){
     this.showInput=true;
+    this.devices.push(this.title, this.descr);
+    this.ngOnInit();
   }
 }
 
