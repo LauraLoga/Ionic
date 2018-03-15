76bcc0dd89096254d57922cf4153c108f66c62d2
Laura <lau@gmail.com>
2018-03-15 14:25:01 +0100

    [STYLE] cards of sensors

--- src/pages/sensors/sensors.ts
+++ src/pages/sensors/sensors.ts
@@ -7,7 +7,7 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SensorsPage {
   selectedItem: any;
-  sensorsList: Array<{ title: string, note: string, icon: string, color: string }>;
+  sensorsList: Array<{ title: string, note: string, icon: string, color: string, attr: Array<{}> }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
@@ -15,11 +15,11 @@ export class SensorsPage {
 
     // Let's populate this page with some filler content for funzies
     this.sensorsList = [
-      { title: 'Luminity',note: 'Measures the luminosity of the environment',icon:'bulb',color: '#FFD439' },
-      { title: 'Rain',note: 'Able to perceive if it is raining to be able to perform the corresponding action',icon:'rainy',color: '#3575AC' },
-      { title: 'Temperature',note: 'Measures the environmental temperature',icon:'thermometer',color: '#F46529' },
-      { title: 'Clock',note: 'Clock that measures time',icon:'time',color: '#412159' },
-      { title: 'Humidity',note: 'Measures the humidity of the environment',icon:'water',color: '#0CA9EA'}
+      { title: 'Luminity',note: 'Measures the luminosity of the environment',icon:'bulb',color: '#FFD439', attr:['IluminacionHabitaculo','IluminacionExterior'] },
+      { title: 'Rain',note: 'Able to perceive if it is raining to be able to perform the corresponding action',icon:'rainy',color: '#3575AC', attr:['HumedadExterior'] },
+      { title: 'Temperature',note: 'Measures the environmental temperature',icon:'thermometer',color: '#F46529', attr:['TemperaturaHabitaculo','TemperaturaExterior'] },
+      { title: 'Clock',note: 'Clock that measures time',icon:'time',color: '#412159', attr:['Hora','Alarma'] },
+      { title: 'Humidity',note: 'Measures the humidity of the environment',icon:'water',color: '#0CA9EA', attr:['HumedadHabitaculo','HumedadCultivo']}
     ];
     
     
